a9be0dfe6b2e37dfe5b78236ceb88d25a11539cf
Monarckz <Facundoalvarez854@gmail.com>
2025-06-28 20:34:30 -0300

    Subo cambios al app.jsx

--- Defensa-carpetas/src/App.jsx
+++ Defensa-carpetas/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { HOME,CREAR,VER,EDITAR } from './Routers/router';
+import { HOME,CREAR,VER,EDITAR,SOBRE } from './Routers/router';
 
 import CreatePage from "./Pages/CreatePage";
 import EditPage from "./Pages/EditPage";
 import Home from "./Pages/Home"
 import ViewPage from "./Pages/ViewPage"
+import SobrePage from './Pages/SobrePage';
 
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
             <Route path={CREAR} element={<CreatePage/>} />
             <Route path={EDITAR} element={<EditPage/>} />
             <Route path={VER} element={<ViewPage/>} />
+            <Route path={SOBRE} element={<SobrePage/>} />
         </Routes>
       </BrowserRouter>
   )
